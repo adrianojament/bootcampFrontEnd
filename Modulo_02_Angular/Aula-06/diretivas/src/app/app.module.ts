e4aa75b85f1ccb1326fdb3dd344fcaab28f101eb
adriano.ament <adrianjoament@gmail.com>
2020-10-06 22:12:17 -0300

    Finalizada todas as aulas da primeira etapa

--- Modulo_02_Angular/Aula-06/diretivas/src/app/app.module.ts
+++ Modulo_02_Angular/Aula-06/diretivas/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoListComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Modulo_02_Angular/Aula-06/diretivas/src/app/todo-list/todo-list.component.ts
+++ Modulo_02_Angular/Aula-06/diretivas/src/app/todo-list/todo-list.component.ts
@@ -7,15 +7,16 @@ import { TodoItem } from './todoItem';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
+  newTask: string = '';
   tasks: TodoItem[] = [
     { description: 'Arrumar a cama', done: false },
     { description: 'Escovar os dentes', done: true },
     { description: 'Tomar Cafe', done: false },
   ];
 
-  addTask(description: string): void {
-    this.tasks = [...this.tasks, { description: description, done: false }];
-    console.log(this.tasks);
+  addTask(): void {
+    this.tasks = [...this.tasks, { description: this.newTask, done: false }];
+    this.newTask = '';
   }
 
   removeTask(index: number): void {
